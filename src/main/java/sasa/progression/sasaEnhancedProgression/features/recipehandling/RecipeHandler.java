5806610b00ce5d005a6a58eedb7298ec8e0f36f3
SasaSuppenknecht <sascha.sashof.hofmann@fau.de>
2026-01-04 16:29:56 +0100

    Set Java Version to 21

--- src/main/java/sasa/progression/sasaEnhancedProgression/features/recipehandling/RecipeHandler.java
+++ src/main/java/sasa/progression/sasaEnhancedProgression/features/recipehandling/RecipeHandler.java
@@ -3,13 +3,10 @@ package sasa.progression.sasaEnhancedProgression.features.recipehandling;
 import org.bukkit.Bukkit;
 import org.bukkit.Keyed;
 import org.bukkit.NamespacedKey;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Recipe;
-import org.bukkit.plugin.PluginManager;
-import sasa.progression.sasaEnhancedProgression.SasaEnhancedProgression;
 import sasa.progression.sasaEnhancedProgression.features.AdvancementUnlockEvent;
 
 import java.util.List;
